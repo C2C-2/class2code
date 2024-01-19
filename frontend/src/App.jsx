b6b2d983d0a908c384051f321815648519134183
alkobr-55 <osama2002w29@gmail.com>
2024-01-19 17:34:42 +0200

    newCode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,26 @@ import { MantineProvider } from "@mantine/core";
 import { SideBar } from "./User/Features/AI Chat/Compnents/SideBar/SideBar";
 import { Main } from "./User/Features/AI Chat/Compnents/Main/Main";
 import { AIChat } from "./User/Features/AI Chat/AIChat";
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 function App() {
-  return <MantineProvider>
-     {/* <SideBar/> 
-    <Main/> */}
-    <AIChat/>
-  </MantineProvider>;
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:3000/graphql",
+  });
+  return (
+    <ApolloProvider client={client}>
+      <MantineProvider>
+        {/* <SideBar/> 
+         <Main/> */}
+        <AIChat />
+      </MantineProvider>
+    </ApolloProvider>
+  );
 }
 
 export default App;
