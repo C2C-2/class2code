536f2f41ad8956f8e653929724b7428395223fbf
abc <abc@example.com>
2024-02-21 19:04:53 +0200

    v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,5 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import { SideBar } from "./User/Features/AI Chat/Compnents/SideBar/SideBar";
-import { Main } from "./User/Features/AI Chat/Compnents/Main/Main";
-import { AIChat } from "./User/Features/AI Chat/AIChat";
-import { LogIn } from "./User/Features/AI Chat/Compnents/Login/LogIn";
-import SignUp from "./User/Features/AI Chat/Compnents/Signup/SignUp";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 function App() {
   const client = new ApolloClient({
@@ -13,13 +8,7 @@ function App() {
   });
   return (
     <ApolloProvider client={client}>
-      <MantineProvider>
-        {/* <SideBar/> 
-         <Main/> */}
-        {/* <AIChat /> */}
-        {/* <LogIn></LogIn> */}
-        <SignUp />
-      </MantineProvider>
+      <MantineProvider></MantineProvider>
     </ApolloProvider>
   );
 }
