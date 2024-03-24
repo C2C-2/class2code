de53190b53fe4b2ea03f2467f86d7a8f600d2428
abc <abc@example.com>
2024-03-24 14:24:46 +0200

    v0.1.1

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Icon } from '@iconify/react'
-import { FaPlus } from "react-icons/fa";
+import { FaPlus, FaArrowLeft } from "react-icons/fa";
 import { TextInput } from '@mantine/core';
+import './Chat.css';
 
 const Chat = () => {
     return (
-        <div className='chat d-flex'>
-            <div className='chat_chats d-flex flex-column'>
-                <div className='chat_chats_head d-flex'>
-                    <button type="button" className="btn btn-dark">Dark</button>
+        <div className='chat d-flex p-4 bg-light gap-4 justify-content-center'>
+            <div className='chat_chats d-flex flex-column col-4 shadow bg-white'>
+                <div className='chat_chats_head d-flex justify-content-around align-items-center'>
+                    <button type="button" className="return-back-btn btn btn-dark">
+                        <FaArrowLeft size={16}/>
+                    </button>
                     <h4>Chats</h4>
-                    <FaPlus />
+                    <FaPlus color='white' className='plus' />
                 </div>
-                <div className='chat_chats_body'>
+                <div className='chat_chats_body px-4'>
                     <TextInput
                         withAsterisk
                         placeholder="search"
@@ -23,7 +25,16 @@ const Chat = () => {
                     </div>
                 </div>
             </div>
-            <div className='chat_messages'></div>
+            <div className='chat_messages col-7 shadow p-4 bg-white'>
+                <div className='chat_messages_head d-flex'>
+                    <h4>User 1</h4>
+                    <p> . online</p>
+                </div>
+                <div className='chat_messages_body'>
+                    <MessageItem userName="User 1" message="Last message" />
+                    <MessageItem userName="User 1" message="Last message" />
+                </div>
+            </div>
         </div>
     )
 }
@@ -33,6 +44,15 @@ const ChatItem = ({ userName, lastMessage }: { userName: string, lastMessage: st
         <div className='d-flex flex-column'>
             <h6 style={{ fontWeight: 'bold' }}>{userName}</h6>
             <p>{lastMessage}</p>
+        </div >
+    )
+}
+
+const MessageItem = ({ userName, message }: { userName: string, message: string }) => {
+    return (
+        <div className='d-flex'>
+            <h6 style={{ fontWeight: 'bold' }}>{userName}</h6>
+            <p>{message}</p>
         </div >
     )
 }
